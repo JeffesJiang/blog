bd1e7b98edf91ee1243192e9b77568ec3e1e1458
JeffesJiang <973913423@qq.com>
2021-08-19 18:03:39 +0800

    feat(public): favicon.ico
    favicon.ico添加

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,11 +7,13 @@ export default defineUserConfig<DefaultThemeOptions>({
     '/': {
       lang: 'zh-CN',
       title: '前端学习成长历程',
+      head: [['link', {rel: 'icon', href: '/favicon.ico'}]],
       description: '姜金甫的个人网站，前端学习，算法学习，GIS，Mapbox学习，Mapbox离线版本，Mapbox离线版本部署，Webgl,three.js',
     },
     '/en/': {
       lang: 'en-US',
       title: 'FRONT END LEARNING GROWTH PROCESS',
+      head: [['link', {rel: 'icon', href: '/favicon.ico'}]],
       description: '姜金甫的个人网站，前端学习，算法学习，GIS，Mapbox学习，Mapbox离线版本，Mapbox离线版本部署，Webgl,three.js',
     }
   },
@@ -38,3 +40,4 @@ export default defineUserConfig<DefaultThemeOptions>({
     }
   }
 });
+
